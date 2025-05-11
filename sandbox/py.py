70f7b61b85d5e36745b51bf44b7b0acaefcd195e
hedonicadapter <mailservice.samherman@gmail.com>
2025-05-11 10:24:29 +0200

    sandbox

---
+++ sandbox/py.py
@@ -0,0 +1,64 @@
+import logging
+import os
+import json
+import traceback
+import base64
+import sys
+
+parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+sys.path.insert(0, parent_dir)
+
+from parser import generate_openapi_schema
+
+file_path = './test.json'
+
+def decodeHcl(encoded_content):
+    decoded_bytes = base64.b64decode(encoded_content)
+    return decoded_bytes.decode('utf-8')
+
+with open(file_path, 'r') as file:
+    data = json.load(file)
+
+    specs = {}
+    contents = data.get("contents", {})  # Safely get the "contents" dictionary
+    for key, encoded_content in contents.items():
+        try:
+            decoded_hcl = decodeHcl(encoded_content) 
+            spec = generate_openapi_schema(decoded_hcl)
+            # print(key)
+            specs[key] = spec
+        except UnicodeDecodeError as e:
+            logging.error(f"Base64 decoding failed for {key}: {str(e)}")
+        except Exception as e:
+            logging.error(f"Processing failed for {key}: {str(e)}")
+            logging.error(traceback.format_exc())
+
+    # print(json.dumps(specs, indent=4))
+
+    api_spec = {
+        "openapi": "3.0.0", 
+        "info": {
+            "title": "Request Subscription API",
+            "version": "not-implemented"
+        },
+        "paths": {
+            "/requestSubscription": {
+                "post": {
+                    "summary": "Request a subscription",
+                    "requestBody": {
+                        "content": {
+                            "application/json": {
+                                "schema": specs
+                            }
+                        }
+                    },
+                    "responses": {
+                        "201": {
+                            "description": "Subscription requested successfully"
+                        }
+                    }
+                }
+            }
+        }
+    }
+    # print(api_spec)
